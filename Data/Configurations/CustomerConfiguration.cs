b75b3ecdd971bdc34c789222e0aee38d4feffdb3
Ziad Mohammed <ziadmohammed613@gmail.com>
2026-03-19 21:10:41 +0200

    CustomerConfiguration.cs done

--- Data/Configurations/CustomerConfiguration.cs
+++ Data/Configurations/CustomerConfiguration.cs
@@ -8,7 +8,20 @@ namespace ShopEasy.Data.Configurations
     {
         public void Configure(EntityTypeBuilder<Customer> builder)
         {
-            
+            builder.ToTable(name: "Customers", schema: "shop");
+            builder.HasKey(c => c.CustomerId).HasName("customer_id");
+            builder.Property(c => c.FullName)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .HasColumnName("full_name")
+                    .HasComment("Customer full legal name");
+            builder.Property(c => c.Email)
+                    .HasMaxLength(250);
+            builder.HasIndex(c => c.Email);
+            builder.Property(c => c.PhoneNumber)
+                    .IsRequired(false)
+                    .HasMaxLength(20);
+            builder.Property(c => c.CreatedAt).HasDefaultValueSql("GETUTCDATE()");
         }
     }
 }
