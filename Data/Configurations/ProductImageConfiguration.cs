a56ec80dd8f2441f4b138cb2a0f7dd8f290df0ea
Ziad Mohammed <ziadmohammed613@gmail.com>
2026-03-20 03:47:33 +0200

    ProductImageConfiguration.cs done

--- Data/Configurations/ProductImageConfiguration.cs
+++ Data/Configurations/ProductImageConfiguration.cs
@@ -7,7 +7,22 @@ namespace ShopEasy.Data.Configurations
     {
         public void Configure(EntityTypeBuilder<ProductImage> builder)
         {
-            
+            builder.ToTable("ProductImages", schema: "shop");
+
+            builder.Property(pi => pi.Url)
+                .IsRequired()
+                .HasMaxLength(500);
+ 
+            builder.Property(pi => pi.AltText)
+                .HasMaxLength(200);
+
+            builder.Property(pi => pi.IsPrimary)
+                .HasDefaultValue(false);
+
+            builder.HasOne(pi => pi.Product)
+                .WithOne(p => p.ProductImage)
+                .HasForeignKey<ProductImage>(pi => pi.ProductId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
